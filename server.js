1286e8f186ac27fecb6be6d17a97170ee7ac2613
Georgi Varbanov <georgi.varbanov@hds-group.local>
2017-07-11 10:02:39 +0300

    Add new page projects

--- server.js
+++ server.js
@@ -44,6 +44,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects page',
+    message: 'Projects portfolio'
+  });
+});
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
